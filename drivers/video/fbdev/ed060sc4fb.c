8102580ef5b3ca4166dd1bf1bc431e333ece7de5
Homin Lee <homin.lee@suapapa.net>
2016-02-26 16:50:22 +0900

    move par struct to header

--- drivers/video/fbdev/ed060sc4fb.c
+++ drivers/video/fbdev/ed060sc4fb.c
@@ -52,6 +52,159 @@ static struct fb_var_screeninfo ed060sc4fb_var = {
 
 /* main ed060sc4fb functions */
 
+static void ed060sc4_vclk(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_ckv, 1);
+	udelay(1)
+	gpio_set_value(par->gpio_ckv, 0);
+	udelay(4)
+}
+
+static void ed060sc4_hclk(struct ed060sc4fb_par *par)
+{
+	ndelay(40);
+	gpio_set_value(par->gpio_cl, 1);
+	ndelay(40);
+	gpio_set_value(par->gpio_cl, 0);
+}
+
+static void ed060sc4_vscan_start(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_gmode, 1);
+	ed060sc4_vclk(par);
+	gpio_set_value(par->gpio_spv, 0);
+	ed060sc4_vclk(par);
+	gpio_set_value(par->gpio_spv, 1);
+	ed060sc4_vclk(par);
+}
+
+static void ed060sc4_vscan_write(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_ckv, 1);
+	gpio_set_value(par->gpio_oe, 1);
+	udelay(5);
+	gpio_set_value(par->gpio_oe, 0);
+	gpio_set_value(par->gpio_ckv, 0);
+	udelay(200);
+}
+
+static void ed060sc4_vscan_bulkwrite(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_ckv, 1);
+	udelay(20);
+	gpio_set_value(par->gpio_ckv, 0);
+	udelay(200);
+}
+
+static void ed060sc4_vscan_skip(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_ckv, 1);
+	udelay(1);
+	gpio_set_value(par->gpio_ckv, 0);
+	udelay(100);
+}
+
+static void ed060sc4_vscan_stop(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_gmode, 0);
+	ed060sc4_vclk(par);
+	ed060sc4_vclk(par);
+	ed060sc4_vclk(par);
+	ed060sc4_vclk(par);
+	ed060sc4_vclk(par);
+}
+
+static void ed060sc4_hscan_start(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_le, 0);
+	gpio_set_value(par->gpio_oe, 0);
+
+	gpio_set_value(par->gpio_sph, 0);
+}
+
+static void ed060sc4_hscan_write(struct ed060sc4fb_par *par,
+				 const uint8_t *data, int count)
+{
+	int i;
+	uint8_t d;
+
+	while (count--) {
+		d = *data++;
+		for(i = 0; i < 8; i++) {
+			gpio_set_value(par->gpio_data[i], (d & (1<<i)) ? 1 : 0);
+		}
+		ed060sc4_hclk(par);
+	}
+}
+
+static void ed060sc4_hscan_stop(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_sph, 1);
+	ed060sc4_hclk(par);
+
+	gpio_set_value(par->gpio_le, 1);
+	ndelay(40);
+	gpio_set_value(par->gpio_le, 0);
+}
+
+static void ed060sc4_power_on(struct ed060sc4fb_par *par)
+{
+	int i;
+
+	gpio_set_value(par->gpio_vdd, 1);
+
+	gpio_set_value(par->gpio_le, 0);
+	gpio_set_value(par->gpio_oe, 0);
+	gpio_set_value(par->gpio_cl, 0);
+	gpio_set_value(par->gpio_sph, 1);
+
+	for (i = 0; i < 8; i++) {
+		gpio_set_value(par->gpio_data[i], 0);
+	}
+
+	gpio_set_value(par->gpio_ckv, 0);
+	gpio_set_value(par->gpio_gmode, 0);
+	gpio_set_value(par->gpio_spv, 1);
+
+	msleep(100);
+
+	gpio_set_value(par->gpio_vneg, 1);
+	msleep(1000);
+	gpio_set_value(par->gpio_vpos, 1);
+
+	ed060sc4_vscan_start(par);
+	for (i = 0; i < 800; i++)
+		ed060sc4_vclk(par);
+	
+	ed060sc4_vscan_stop(par);
+}
+
+
+static void ed060sc4_power_off(struct ed060sc4fb_par *par)
+{
+	gpio_set_value(par->gpio_vpos, 0);
+	gpio_set_value(par->gpio_vneg, 0);
+
+	msleep(100);
+
+	gpio_set_value(par->gpio_le, 0);
+	gpio_set_value(par->gpio_oe, 0);
+	gpio_set_value(par->gpio_cl, 0);
+	gpio_set_value(par->gpio_sph, 0);
+
+	for (i = 0; i < 8; i++) {
+		gpio_set_value(par->gpio_data[i], 0);
+	}
+
+	gpio_set_value(par->gpio_ckv, 0);
+	gpio_set_value(par->gpio_gmode, 0);
+	gpio_set_value(par->gpio_spv, 0);
+
+	gpio_set_value(par->gpio_vdd, 0);
+}
+
+
+
 static void ed060sc4fb_dpy_update(struct ed060sc4fb_par *par)
 {
 	int i, x, y;
@@ -186,7 +339,6 @@ static struct fb_deferred_io ed060sc4fb_defio = {
 static int ed060sc4fb_probe(struct platform_device *dev)
 {
 	struct fb_info *info;
-	struct ed060sc4_board *board;
 	int retval = -ENOMEM;
 	int videomemorysize;
 	unsigned char *videomemory;
--- include/video/ed060sc4fb.h
+++ include/video/ed060sc4fb.h
@@ -1,51 +1,35 @@
 /*
  * ed060sc4fb.h - definitions for the ed060sc4 framebuffer driver
  *
- * Copyright (C) 2008 by Jaya Kumar
+ * Copyright (C) 2016 by Homin Lee <homin.lee@suapapa.net>
  *
- * This file is subject to the terms and conditions of the GNU General Public
- * License. See the file COPYING in the main directory of this archive for
- * more details.
+ *  This program is free software; you can redistribute  it and/or modify it
+ *  under  the terms of  the GNU General  Public License as published by the
+ *  Free Software Foundation;  either version 2 of the  License, or (at your
+ *  option) any later version.
  *
  */
 
-#ifndef _LINUX_HECUBAFB_H_
-#define _LINUX_HECUBAFB_H_
+#ifndef _LINUX_ED060SC4FB_H_
+#define _LINUX_ED060SC4FB_H_
 
-/* Apollo controller specific defines */
-#define APOLLO_START_NEW_IMG	0xA0
-#define APOLLO_STOP_IMG_DATA	0xA1
-#define APOLLO_DISPLAY_IMG	0xA2
-#define APOLLO_ERASE_DISPLAY	0xA3
-#define APOLLO_INIT_DISPLAY	0xA4
+/* struct used by ed060sc4 */
 
-/* Hecuba interface specific defines */
-#define HCB_WUP_BIT	0x01
-#define HCB_DS_BIT 	0x02
-#define HCB_RW_BIT 	0x04
-#define HCB_CD_BIT 	0x08
-#define HCB_ACK_BIT 	0x80
-
-/* struct used by ed060sc4. board specific stuff comes from *board */
 struct ed060sc4fb_par {
 	struct fb_info *info;
-	struct ed060sc4_board *board;
-	void (*send_command)(struct ed060sc4fb_par *, unsigned char);
-	void (*send_data)(struct ed060sc4fb_par *, unsigned char);
-};
 
-/* board specific routines
-board drivers can implement wait_for_ack with interrupts if desired. if
-wait_for_ack is called with clear=0, then go to sleep and return when ack
-goes hi or if wait_for_ack with clear=1, then return when ack goes lo */
-struct ed060sc4_board {
-	struct module *owner;
-	void (*remove)(struct ed060sc4fb_par *);
-	void (*set_ctl)(struct ed060sc4fb_par *, unsigned char, unsigned char);
-	void (*set_data)(struct ed060sc4fb_par *, unsigned char);
-	void (*wait_for_ack)(struct ed060sc4fb_par *, int);
-	int (*init)(struct ed060sc4fb_par *);
-};
+	int gpio_ckv;
+	int gpio_cl;
+	int gpio_gmode;
+	int gpio_gpv;
+	int gpio_oe;
+	int gpio_le;	
+	int gpio_sph;
+	int gpio_data[8];
 
+	int gpio_vdd;
+	int gpio_vpos;
+	int gpio_vneg;
+}
 
 #endif
